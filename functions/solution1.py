794b66b860ec5959e104bd1b0815e2fda4cb2f0c
Your Name <your-email@example.com>
2025-11-22 22:00:35 +0530

    completed

--- functions/solution1.py
+++ functions/solution1.py
@@ -1,3 +1,9 @@
-# code for the binary searching 
-print("hello")
+#  function with multiple parameters
+#creating a function that take two nos. as parameters and return there sum 
 
+def sum(a,b):
+    return(a+b)
+
+a=int(input("enter a no : "))
+b=int(input("enter a no : "))
+print(sum(a,b))
---
+++ functions/solution2.py
@@ -0,0 +1,3 @@
+# polymorphism in functions
+# a function  multiply that multiplies two nos. ,but can also accept and multiply strings
+
