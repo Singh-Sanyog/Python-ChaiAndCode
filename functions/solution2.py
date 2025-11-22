5049b9eaf597328ad11e5ca826352b39d07e595d
Your Name <your-email@example.com>
2025-11-22 22:14:22 +0530

    all done

--- functions/solution2.py
+++ functions/solution2.py
@@ -1,3 +1,9 @@
 # polymorphism in functions
 # a function  multiply that multiplies two nos. ,but can also accept and multiply strings
 
+def multiply(container1,container2):
+    return(container1*container2)
+
+print(multiply(8,8))
+print(multiply("a",8))
+print(multiply(8,"a"))
