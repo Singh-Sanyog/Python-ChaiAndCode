26f39227d63136907e4dec9018ce0039b925fe9e
Your Name <your-email@example.com>
2025-11-23 14:23:13 +0530

    functions done

--- functions/solution6.py
+++ functions/solution6.py
@@ -1,4 +1,11 @@
 # function with **kwargs
 # function that accepts any no. of keyword arguments and prints them in the format key:value.
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}:{value}")
+        
 
- 
+print_kwargs(power="lazer",name="shaktiman")
+print_kwargs(power="lazer")
+print_kwargs(name="shaktiman")
+print_kwargs(power="lazer",name="shaktiman",enenmy="dr. jackal")
---
+++ functions/solution7.py
@@ -0,0 +1,10 @@
+# generator function with yield  
+# write a generator(keyword - generate value) function taht yield even numbers up to a specific limit 
+
+def even_generator(limit):
+    for i in range (2,limit+1,2):
+        yield i
+        
+       
+for num in even_generator(10):
+    print(num)
